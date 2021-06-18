6ca230217ad28be930da4f369839e5feee6dbb2c
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-06-18 22:40:15 +0300

    Added Main component.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import Container from "./components/Container/Container";
 import AppBarTop from "./components/AppBarTop/AppBarTop";
 import { ReactComponent as ReactLogo } from "./assets/logo.svg";
+import Main from "./components/Main/Main";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <ReactLogo />
         </Container>
       </AppBarTop>
+      <Main></Main>
     </>
   );
 }
